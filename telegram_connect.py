3e840a91e5529ec09a4143ab0c8669ffce3e30ee
Sammels <Sasagete@gmail.com>
2023-04-14 10:29:49 +0300

    Update telegram_connect.py

--- telegram_connect.py
+++ telegram_connect.py
@@ -7,15 +7,6 @@ import telegram
 from dotenv import load_dotenv
 
 
-def parse_arguments() -> object:
-    """Function parsing CLI arguments"""
-    parser = argparse.ArgumentParser(description="Программа скачивает файлы.")
-    parser.add_argument("-t", "--time", help="Пауза в отправке сообщений (час)",
-                        type=int, default=4)
-    args = parser.parse_args()
-    return args
-
-
 def walk_dir(path: str):
     """Use OS.Walk to take info about dir"""
     for root, dirs, files in os.walk(f"{path}"):
@@ -40,8 +31,12 @@ if __name__ == "__main__":
         updates = bot.get_updates()
 
         file = walk_dir(path)
-        arguments = parse_arguments()
+
+        parser = argparse.ArgumentParser(description="Программа скачивает файлы.")
+        parser.add_argument("-t", "--time", help="Пауза в отправке сообщений (час)",
+                            type=int, default=4)
+        args = parser.parse_args()
 
         send_image(chat_id)
-        pause_time = (arguments.time * 60) * 60
+        pause_time = (args.time * 60) * 60
         time.sleep(pause_time)
